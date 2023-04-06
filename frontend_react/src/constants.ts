391fd12d2a2557330fafdd5c5f956a8550bc5dd0
dany002 <moldovandangelu@gmail.com>
2023-04-06 23:37:44 +0300

    initial commit

--- frontend_react/src/constants.ts
+++ frontend_react/src/constants.ts
@@ -1,3 +1,4 @@
-const DEV_BACKEND_API_URL = "http://ec2-18-192-105-217.eu-central-1.compute.amazonaws.com/api";
+//const DEV_BACKEND_API_URL = "http://ec2-18-192-105-217.eu-central-1.compute.amazonaws.com/api";
+const DEV_BACKEND_API_URL = "/api";
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL
