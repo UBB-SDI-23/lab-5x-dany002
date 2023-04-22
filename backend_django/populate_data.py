9f798136d5f58ba9bd162e6d5a9798baed2720a3
dany002 <moldovandangelu@gmail.com>
2023-04-22 17:42:04 +0300

    initial commit

--- backend_django/populate_data.py
+++ backend_django/populate_data.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     with open('populate_team.csv', 'w') as f:
         # f.write('INSERT INTO employee_team(created,"nameOfTeam","freePlaces",purpose,admin,rating) VALUES \n')
         for i in range(n):
-            f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.text(max_nb_chars=100) + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '\n')
+            f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.text(max_nb_chars=50).replace('\n',' ') + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '\n')
             print(i,"/",n)
     #     f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '),\n')
         # f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1, 10).__str__() + ');\n')
