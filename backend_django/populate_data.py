7dcb9a39feda743fbfc1cdca889c82081f2cc1c0
dany002 <moldovandangelu@gmail.com>
2023-04-22 15:51:04 +0300

    initial commit

--- backend_django/populate_data.py
+++ backend_django/populate_data.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     n = 5
     with open('populate_team.sql', 'w') as f:
         f.write('INSERT INTO employee_team(created,nameOfTeam,freePlaces,purpose,admin,rating) VALUES \n')
-        for _ in range(n):
-            fake.name()
-            f.write('(' + date.today().strftime("%d/%m/%Y") + ',' + fake.name() + ',' + random.randint(0,100).__str__() + ',' + fake.paragraph() + "," + fake.name() + "," + random.randint(1,10).__str__() + '\n')
+        for _ in range(n-1):
+            f.write("('" + date.today().strftime("%d/%m/%Y") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '),\n')
+        f.write("('" + date.today().strftime("%d/%m/%Y") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1, 10).__str__() + ');\n')
--- backend_django/populate_team.sql
+++ backend_django/populate_team.sql
@@ -1,6 +1,6 @@
 INSERT INTO employee_team(created,nameOfTeam,freePlaces,purpose,admin,rating) VALUES 
-(22/04/2023,Joshua Marshall,24,Scene remember include threat our because drive. Detail indicate course letter serve direction color.,Jared Cooper,1
-(22/04/2023,Christina Williamson,27,Thing significant act indeed heart. Event difference book whatever bed son.,Lisa Taylor,10
-(22/04/2023,Megan Taylor,5,Before investment yourself win like. Human method send whatever successful idea. Magazine official certainly decision no they effect.,Brian Leblanc,3
-(22/04/2023,Felicia Guzman,77,Improve probably like skill admit never. Other ask poor rich brother like. Point method officer area media learn shake.,Kimberly Galloway,3
-(22/04/2023,Emily Peterson,60,Arrive travel investment attorney. South bring technology term well kind. Form bank catch investment few finally win.,Stacy Hernandez,4
+('22/04/2023','Matthew Bowers',55,'Receive end break job size realize. Condition carry explain reveal nation. Win it police represent difference effort fish. Throughout raise nice check prepare close subject table.','Robert Robinson',1),
+('22/04/2023','Mark Willis',42,'Reach feel tax shoulder foreign story. Popular call form. Candidate require doctor argue site recently certain. Material mean item sound role rise capital.','Susan Cabrera',1),
+('22/04/2023','Ryan Mcdaniel',0,'Social follow office mention brother.','Vincent Johnson',1),
+('22/04/2023','Danny Greene',84,'Respond example send travel foot court. Group season shoulder.','James Sweeney',4),
+('22/04/2023','Timothy Franklin',83,'Continue guess network production coach woman road character. Difficult direction environmental between. Rich player whom billion building forget suffer six. Myself now study property national number discuss.','Amanda Harris',3);
