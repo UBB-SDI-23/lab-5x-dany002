17ee099f93e8ffcc44970843ba2c6255beb11c46
dany002 <moldovandangelu@gmail.com>
2023-04-22 16:53:11 +0300

    initial commit

--- backend_django/populate_data.py
+++ backend_django/populate_data.py
@@ -7,9 +7,13 @@ if __name__ == "__main__":
     from faker import Faker
 
     fake = Faker()
-    n = 1000000
+    n = 5
     with open('populate_team.sql', 'w') as f:
-        f.write('INSERT INTO employee_team(created,"nameOfTeam","freePlaces",purpose,admin,rating) VALUES \n')
-        for _ in range(n-1):
-            f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '),\n')
-        f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1, 10).__str__() + ');\n')
+        # f.write('INSERT INTO employee_team(created,"nameOfTeam","freePlaces",purpose,admin,rating) VALUES \n')
+        for _ in range(n):
+            f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '\n')
+
+    #     f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '),\n')
+        # f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1, 10).__str__() + ');\n')
+        #
+        #
