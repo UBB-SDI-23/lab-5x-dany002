c85932a3448da3f8204820af95a6d6bb71fd0581
dany002 <moldovandangelu@gmail.com>
2023-04-22 16:57:19 +0300

    initial commit

--- backend_django/populate_data.py
+++ backend_django/populate_data.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     from faker import Faker
 
     fake = Faker()
-    n = 5
-    with open('populate_team.sql', 'w') as f:
+    n = 1000000
+    with open('populate_team.csv', 'w') as f:
         # f.write('INSERT INTO employee_team(created,"nameOfTeam","freePlaces",purpose,admin,rating) VALUES \n')
         for _ in range(n):
             f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '\n')
