4a74d9ef1d04ea970562b1ccb8530f46f8310d60
dany002 <moldovandangelu@gmail.com>
2023-04-22 16:25:24 +0300

    initial commit

--- backend_django/populate_team.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO employee_team(created,"nameOfTeam","freePlaces",purpose,admin,rating) VALUES 
-('2023-04-22','Laura Quinn',39,'Again anyone window effort. Move country reason no material picture. Music daughter single kind will member.','Kimberly Andrews',8),
-('2023-04-22','Jamie Frederick',69,'Interest focus compare customer old notice. Blue pull a matter two occur person. Career himself believe five door pattern direction back.','Sherry Randall',8),
-('2023-04-22','Karen Walsh',6,'Factor serious artist act staff. Cost senior lose director note pay. However beautiful true message attorney. Often often worry possible century girl.','Catherine Gilbert',2),
-('2023-04-22','Katherine Carpenter',8,'Town high when lead space tonight away may. Fly old raise.','Randall Hancock',7),
-('2023-04-22','Gabrielle James',26,'Less back Mrs able. Piece sister light each TV go see two. Long summer project represent film.','Donna Jones',5);
