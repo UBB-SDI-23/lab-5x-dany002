b8d7e1da5e6250191270bf2261933ebd6c52ef1c
dany002 <moldovandangelu@gmail.com>
2023-04-23 18:42:37 +0300

    initial commit

--- backend_django/populate_data.py
+++ backend_django/populate_data.py
@@ -8,18 +8,19 @@ if __name__ == "__main__":
 
     fake = Faker()
     n = 1000000
-    # with open('populate_team.csv', 'w') as f:
-    #     # f.write('INSERT INTO employee_team(created,"nameOfTeam","freePlaces",purpose,admin,rating) VALUES \n')
-    #     for i in range(n):
-    #         f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.text(max_nb_chars=50).replace('\n',' ') + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '\n')
-    #         print(i,"/",n)
-    # #     f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '),\n')
-    #     # f.write("('" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.paragraph() + "','" + fake.name() + "'," + random.randint(1, 10).__str__() + ');\n')
-    #     #
-    #     #
+    with open('populate_team.csv', 'w') as f:
+        for i in range(n):
+            f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(0,100).__str__() + ",'" + fake.text(max_nb_chars=50).replace('\n',' ') + "','" + fake.name() + "'," + random.randint(1,10).__str__() + '\n')
+            print(i,"/",n)
+
     with open('populate_projects.csv', 'w') as f:
         statues = ['in progress', 'finished', 'pending']
 
         for i in range(n):
             f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "','" + fake.name() + "'," + random.randint(0,100000).__str__() + ",'" + fake.text(max_nb_chars=50).replace('\n',' ') + "','" + fake.random_element(elements=statues) + "'\n")
             print(i, "/", n)
+
+    with open('populate_tasks.csv', 'w') as f:
+        for i in range(10*n):
+            f.write("'" + date.today().strftime("%Y-%m-%d") + "','" + fake.name() + "'," + random.randint(1,10).__str__() + "," + random.randint(1,n).__str__() + "," + random.randint(1,n).__str__() + "\n")
+            print(i, "/", 10*n)
